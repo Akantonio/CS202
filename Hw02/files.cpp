b4b8b35831e49e101aad6388c787db61cb9fb9b3
Akantonio <adrianvantonio@yahoo.com>
2019-10-08 23:10:58 -0800

    finished numberchase

--- Hw02/files.cpp
+++ Hw02/files.cpp
@@ -10,7 +10,7 @@
 int getIdFromFile(const std::string& s, std::istream& is, std::ostream& os)
 {
 	std::ifstream ifile(s);
-	std::string line;
+	std::string linefile;
 	std::string lineInput;
 	getline(is,lineInput);
 	std::string userName;
@@ -25,12 +25,13 @@ int getIdFromFile(const std::string& s, std::istream& is, std::ostream& os)
 		
 		while (true) 
 		{
+			ifile >> userName >> userID;
 			if(userName==userInput)
 			{
 				
 				os << userID;
 			}
-			ifile >> userName >> userID;
+			
 			if (!ifile)
 			{
 				if (ifile.eof())
@@ -53,7 +54,18 @@ int getIdFromFile(const std::string& s, std::istream& is, std::ostream& os)
 void numberChase(const std::string& s, std::ostream& os)
 {
 	int move=0;
-	int temp;
-	std::ifstream inputFile(s,std::ifstream::binary);
+	int read=0;
+	std::ifstream inputFile;
+	inputFile.open(s, std::ios::in | std::ios::binary);
+	while(true)
+	{
 
+		inputFile.seekg(sizeof(int) * move);
+		inputFile.read(reinterpret_cast<char*>(&read), sizeof(int));
+		move = read;
+		if(read<0)
+		{
+			break;
+		}
+	}
 }
